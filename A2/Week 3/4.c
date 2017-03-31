524f2bd4358a26052684e33840ed104d2f62d934
Basilisk1995 <saifullah.abc@gmail.com>
2017-03-31 10:43:27 +0600

    Update and rename 3rd_week_A2/4.c to A2/Week 3/4.c

--- A2/Week 3/4.c
+++ A2/Week 3/4.c
@@ -1,4 +1,8 @@
-///problem 4
+/*
+Problem 4 coded by Saifullah Talukder
+Take an integer as input and print a triangle consisting of that many lines.
+Look at the output to better understand the format of the triangle.
+*/
  
 #include <stdio.h>
  
