370220f7643cad921ded02121372c5ee391c98ba
Mohammad Solaiman <solaimancsebuet16@gmail.com>
2017-04-10 19:57:58 +0600

    Update problem_1.c

--- A1/Week-7/problem_1.c
+++ A1/Week-7/problem_1.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 int A[1000];
 
+/*
+    rec_sum(n) returns the sum of A[n], A[n-1], A[n-2], ... , A[1] , A[0]
+    If we can somehow find out rec_sum(n-1) we can say that
+    rec_sum(n) = A[n] + rec_sum(n-1)
+    
+    However, n = 0 is the base case. When n = 0 it is obvious that rec_sum(0) = A[0]
+    So, we won't 'go down' anymore and return the value
+*/
+
 long int rec_sum (int n)
 {
     if (!n) return A[n];
@@ -23,6 +32,7 @@ int main(void)
     for (i = 0; i < n; i++) scanf("%d", &A[i]);
 
     sum = rec_sum(n-1);
+    // rec_sum(n-1) because A[n-1] is the last element of the array
     printf("The sum of array is %ld\n", sum);
 
     return 0;
