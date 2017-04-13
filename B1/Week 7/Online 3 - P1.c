86bd57241e0553bfe4c85487bcc1b8c874db94a0
Anachor <anachored@gmail.com>
2017-04-13 09:53:32 +0600

    Update Online 3 - P1.c

--- B1/Week 7/Online 3 - P1.c
+++ B1/Week 7/Online 3 - P1.c
@@ -1,5 +1,23 @@
 /*
-Add Problem description here
+You are given a number x and an integer n. You have to write a recursive
+function that calculates x^n (x to the power of n). Your function must be
+based on the parity of n. 
+
+Ask the user for the values of x and n. then find the value of x^n using your
+function, then output it.
+*/
+
+
+/*
+This is our recursive function power.The key idea is 
+if n is even, we can calculate x^n as the square of x^(n/2). 
+Example: x^10 = x^5 * x^5
+
+If n is odd, we just calculate x^(n-1) and multiply it by x. 
+Example: x^7 = x * x^6
+
+Also for negative value of n, we just invert x^-n.
+Example: x^-9 = 1 / x^9
 */
 
 double power (double x, int n)
@@ -11,6 +29,8 @@ double power (double x, int n)
     return k*k;
 }
 
+
+/* just boring io*/
 int main()
 {
     double x;
