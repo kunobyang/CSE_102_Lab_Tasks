773d76505cfdfdea5128f901305a8711d12cd308
Anachor <anachored@gmail.com>
2017-04-26 13:10:56 +0600

    Update Strlen.c

--- B1/Week 8/Strlen.c
+++ B1/Week 8/Strlen.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
 
-int LIM=100;
+int LIM=100;        //Maximum number of chars (buffer)
 
 int strlen (char *s)
 {
-    int res=0;
+    int res=0;      //This saves the length
+    
+    /*At each step we check if the current pointer points to a null
+      character. If so, we stop. else it is valid character. So, we
+      increment res. We also increment pointer s so that it points 
+      to the next character. The loop continues until we hit a null
+      character and adds 1 for each valid character. At the end we 
+      have the length of s in res. */
+      
+      
     while (*s!='\0')
     {
         res++;
