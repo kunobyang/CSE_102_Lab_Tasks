f33c6130f6e4310c8fa2f6c215ddbf3b3e497750
Anachor <anachored@gmail.com>
2017-04-26 12:57:33 +0600

    Update Bitwise Multiplication.c

--- B1/Week 8/Bitwise Multiplication.c
+++ B1/Week 8/Bitwise Multiplication.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 
+
 int main()
 {
     int a, b, i;
-    printf("Enter two numbers: ");
+    printf("Enter two numbers: ");          //IO
     scanf("%d%d", &a, &b);
 
-    int prod=0;
+    int prod=0;                 //This will save the product
 
-    for (i=0; i<32; i++)
+    for (i=0; i<32; i++)        //index of bit
     {
-        if (b&(1<<i))
-            prod+=(a<<i);
+        if (b&(1<<i))           //Check if the ith bit is 1
+            prod+=(a<<i);       //If it is the lshift a by i bits and add to the product
     }
     printf("The product is %d", prod);
 }
